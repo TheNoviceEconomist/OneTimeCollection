920ad63b25cdbb3beb1e855538eab362f31a4ea2
murtazanasafi <nasafi.murtaza@gmail.com>
2018-01-08 08:55:48 -0500

    Intial upload

--- ValidateShapefile.py
+++ ValidateShapefile.py
@@ -15,7 +15,7 @@ class CheckSHP:
 
         # create formatter
         formatter = ('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        fh = logging.basicConfig(filename=r"{}_Log_{}.csv".format(__name__, time.strftime("%Y_%m_%d_%H_%M")),
+        fh = logging.basicConfig(filename=r"{}_Log_{}.csv".format(__name__.replace(".", "_"), time.strftime("%Y_%m_%d_%H_%M")),
                                  level=logging.DEBUG, format=formatter)
 
     def __repr__(self):
@@ -49,7 +49,7 @@ class CheckSHP:
 
                     if self.outpath is None:
 
-                        out_table = r"logs\Geometry_Error_report_{}".format(os.path.basename(fc.strip(".shp")))
+                        out_table = r"\logs\Geometry_Error_report_{}".format(os.path.basename(fc.strip(".shp")))
                         arcpy.CheckGeometry_management(fc, out_table)
                         logging.info(arcpy.GetMessages(0))
 
@@ -85,41 +85,26 @@ class CheckSHP:
 
         for fc in self.fcList:
             dic_key = os.path.basename(fc)
-            print(dic_key)
+            #print(dic_key)
             field_list = arcpy.ListFields(fc)
             field_name = [f.name for f in field_list]
             field_type = [f.type for f in field_list]
             self.field_dic[dic_key]={'fields': list(zip(field_name, field_type))}
 
-            print(self.field_dic[dic_key]["fields"][2:])
 
-            masterList = [('SEQID', 'SmallInteger'),
-                          ('FRN', 'String'),
-                          ('HOCO', 'String'),
-                          ('SOFT', 'String'),
-                          ('DATE', 'String'),
-                          ('SPECTRUM', [ 'SmallInteger', 'LongInteger', 'String' ]),
-                          ('BANDWIDTH', 'SmallInteger'),
-                          ('RSRP', 'SmallInteger')]
-
-            bad_file = False
-
-            fdict = dict(self.field_dic[dic_key]["fields"])
-
-            for fname, ftype in masterList:
-                if fname not in fdict.keys():
-                    bad_file = True
-                elif (isinstance(fdict[fname], list) and ftype not in fdict[fname]) or ftype != fdict[fname]:
-                    bad_file = True
-                else:
-                    bad_file = False
-
-
-
-
-
-            if masterList in self.field_dic[dic_key]["fields"][2:]:
-                print("true")
-            else:
-                print("not true")
-
+    def fields_headers_match(self, headers):
+        assert isinstance(self.field_dic, dict), 'field_dict argument must be a dictionary:%s' % type(self.field_dic)
+        exclude_fields = ['FID', 'Shape']
+        res = True
+        for file, fdict in self.field_dic.items():
+            ftuples = sorted(
+                [(fname.upper(), ftype) for fname, ftype in fdict['fields'] if not fname in exclude_fields])
+            for idx, (hname, htype) in enumerate(headers):
+                if (hname != ftuples[idx][0]) or (
+                    (htype != ftuples[idx][1]) and (isinstance(htype, list) and ftuples[idx][1] not in htype)):
+                    print('mismatch in file:%s field tuple:%s (expected header:%s / allowed type(s):%s)' % (
+                    file, ftuples[idx], hname, htype))
+                    logging.warning('mismatch in file:%s field tuple:%s (expected header:%s / allowed type(s):%s)' % (
+                    file, ftuples[idx], hname, htype))
+                    res = False
+        return res
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from Script import ValidateShapefile
 
-text = ValidateShapefile.CheckSHP(r"D:\FCC_GIS_Projects\MFII\DataCollection\PTI Pacifica\New Submission", "logs")
+text = ValidateShapefile.CheckSHP(r"D:\FCC_GIS_Projects\MFII\DataCollection\PTI Pacifica\New Submission", r"..\logs\sprint")
 
 text.get_shapefile_path_walk()
 
-print(text.fcList)
+#print(text.fcList)
 
 text.check_geom()
 
 text.check_fields()
 
-print(text.field_dic)
+#print(text.field_dic)
+
+headers = sorted([('SEQID', 'SmallInteger'), ('FRN', 'String'), ('HOCO', 'String'), ('SOFT', 'String'),
+                      ('DATE', 'Date'), ('SPECTRUM', ['SmallInteger', 'LongInteger', 'String']),
+                      ('BANDWIDTH', ['SmallInteger', 'LongInteger', 'String']), ('RSRP', 'SmallInteger')])
+
+text.fields_headers_match(headers)
